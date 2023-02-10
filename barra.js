7cd003e2b4c4c45b46691055ce00344530d7e6e9
ch22obsidiana <123427195+ch22obsidiana@users.noreply.github.com>
2023-02-10 13:16:22 -0600

    Add files via upload
    Se añadio barra de navegacion

---
+++ barra.js
@@ -0,0 +1,116 @@
+document.addEventListener('DOMContentLoaded',function(){
+  agregarEncabezado();
+});
+
+function agregarEncabezado(){
+  
+
+        //link.href = 'http://fonts.googleapis.com/css?family=Oswald&effect=neon';
+        
+        let link2 = document.createElement('link');
+        
+        link2.rel = 'stylesheet';
+        link2.href =  href="../css/normalize.css"
+        document.head.appendChild(link2);
+        let link3 = document.createElement('link');
+       
+        link3.rel = 'preconnect';
+        link3.href =  href="https://fonts.googleapis.com"
+        document.head.appendChild(link3);
+
+        let link4 = document.createElement('link');
+        
+        link4.rel = 'stylesheet';
+        link4.href =  href="https://fonts.googleapis.com/css2?family=Suez+One&display=swa";
+        document.head.appendChild(link4);
+
+        let link = document.createElement('link');
+        link.type = 'text/css';
+        link.rel = 'stylesheet';
+        link.href =  href="../css/barra.css"
+        document.head.appendChild(link);
+
+        let script2= document.createElement('script');
+        
+        script2.src = 'https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js';
+        document.head.appendChild(script2);
+
+        let script = document.createElement('script');
+        
+        script.src = '../js/bar.js';
+        document.head.appendChild(script);
+
+        
+
+        
+
+      
+  let nav = document.querySelector('header')
+    nav.innerHTML = `
+   
+    <section class="navigation">
+                <div class="nav-container">
+                  <div class="brand">
+                     <a href="../index.html" class=".nav-centrado logo-cont"><img src="https://res.cloudinary.com/dzham2sp8/image/upload/v1675874139/barra/barra_fkhq8b.png" alt="" class="logo"></a>
+                  </div>
+                  <nav>
+                    <div class="nav-mobile"><a id="nav-toggle" href="#!"><span></span></a></div>
+                    <ul class="nav-list">
+                      <li>
+                        <a href="../index.html">Home</a>
+                      </li>
+                      <li>
+                        <a href="#!">Servicios</a>
+                        <ul class="nav-dropdown">
+                          <li>
+                            <a href="#!">Catalogo</a>
+                          </li>
+                          <li>
+                            <a href="./aboutus.html">Nosotros</a>
+                          </li>
+                          <li>
+                            <a href="#!">Piedra de Nacimiento</a>
+                          </li>
+                          <li>
+                            <a href="./contacto.html">Contacto</a>
+                          </li>
+                        </ul>
+                      </li>
+                      <li class="li-encuentra">
+                        <div class="parteD nav-centrado">
+                            <div class="encuentra">
+                                <a href="#" class="visitado .nav-centrado">¡Encuentra tu piedra!</a>
+                            </div>
+                        </div>
+                      </li>
+                     
+                      <li class="li-iconos">
+                        <div class="parteE nav-centrado">
+                            <div class="iconos">
+                                <a href="##"><img src="https://res.cloudinary.com/dzham2sp8/image/upload/v1675874139/barra/carrito_wbkn0j.svg" alt="" class="icono iconoA"></a>
+                                <a href="#"><img src="https://res.cloudinary.com/dzham2sp8/image/upload/v1675874139/barra/insta_s625xj.svg" alt="" class="icono iconoB"></a>
+                                     <div class="iconoC iconoCBarra">
+                                        <img src="https://res.cloudinary.com/dzham2sp8/image/upload/v1675874139/barra/search_vyjo8i.svg" alt="" class=""><input type="text" class="iconoC" size="1">
+                                    </div>
+                                <a href=""><img src="https://res.cloudinary.com/dzham2sp8/image/upload/v1675874139/barra/login_e3b87g.svg" alt="" class="icono iconoD"></a>
+                                
+                            </div>
+                      </li>
+                      
+                        <li>
+                            <a href="#" class="parteF">¡Encuentra tu piedra!</a>
+                          </li>
+                      
+                        <li>
+                            <a href="#" class="parteF">Contacto</a>
+                          </li>
+                      
+                     
+                    </ul>
+                  </nav>
+                </div>
+              </section>
+              <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+      <script src="../js/bar.js"></script>
+              `
+}
